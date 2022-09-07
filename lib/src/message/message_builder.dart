56343b4c5ab6ecb7d8364389d6c30efe1890ca1f
cyrax <bskaxe@gmail.com>
2022-09-07 14:29:48 +0700

    refactor MulticastMessageBuilder

--- lib/src/message/message_builder.dart
+++ lib/src/message/message_builder.dart
@@ -1,49 +1,27 @@
-import 'message.dart';
+import 'fcm_message.dart';
 import 'message_element.dart';
 
 class MulticastMessageBuilder {
   MulticastMessageBuilder({
     required this.clientTokens,
-    required this.messageElements,
+    required this.fcmMessageBody,
   });
   final List<ClientToken> clientTokens;
-  final List<MessageElement> messageElements;
+  final FcmMessageBody fcmMessageBody;
 
   /// throws [MessageException]
-  List<Message> build() {
-    final builtMessages = <Message>[];
-    final messageWithoutTarget = _constructMessageWithoutTarget();
-    for (final token in clientTokens) {
-      final target = _constructTarget(token);
-      final messageWithTarget =
-          _addTargetToMessage(target: target, message: messageWithoutTarget);
-      final wrappedMessage = ElementWrapper(body: messageWithTarget).build();
-      final builtMessageFromMap = Message.fromMap(wrappedMessage);
-      builtMessages.add(builtMessageFromMap);
+  List<FcmMessage> build() {
+    final builtMessages = <FcmMessage>[];
+    final targetElements =
+        clientTokens.map((token) => TargetElement(token: token));
+    for (final targetElement in targetElements) {
+      builtMessages.add(FcmMessage(
+        targetElement: targetElement,
+        fcmMessageBody: fcmMessageBody,
+      ));
     }
 
     return builtMessages;
-  }
-
-  Map<String, Object> _constructMessageWithoutTarget() {
-    Map<String, Object> constructedMessage = {};
-    for (final element in messageElements) {
-      constructedMessage.addAll(element.build());
-    }
-    return constructedMessage;
-  }
-
-  Map<String, Object> _constructTarget(ClientToken token) {
-    return TargetElement(token: token).build();
-  }
-
-  Map<String, Object> _addTargetToMessage(
-      {required Map<String, Object> target,
-      required Map<String, Object> message}) {
-    Map<String, Object> constructedMessage = <String, Object>{};
-    constructedMessage.addAll(target);
-    constructedMessage.addAll(message);
-    return constructedMessage;
   }
 }
 
